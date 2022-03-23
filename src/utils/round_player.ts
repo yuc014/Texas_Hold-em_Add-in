4cf3679606ba23da3ba2f801dfe0dc3b030fb973
Zhengjie Huang <zhenhuang@microsoft.com>
2022-03-23 18:51:26 +0800

    Modify player class

--- src/utils/round_player.ts
+++ src/utils/round_player.ts
@@ -1,57 +1,22 @@
-import TablePlayer from "./table_player";
+import { nanoid } from "nanoid";
 
 export default class RoundPlayer {
-  #currentBet: number;
+  currentBet: number;
   folded: boolean;
   hand: any[];
   winnings: number;
   takenAction: boolean;
-  tablePlayer: TablePlayer;
-  revealed: boolean;
-  solvedHand?: any;
+  winner: boolean;
+  chips: number;
+  name: any;
+  id: any;
 
-  constructor(tablePlayer: TablePlayer) {
-    this.tablePlayer = tablePlayer;
-    this.folded = false;
+  constructor(name, folded, hand, chips, currentBet) {
+    this.id = nanoid;
+    this.name = name;
+    this.chips = chips; // total chips
+    this.folded = folded;
+    this.currentBet = currentBet; // chips bet for current round
     this.hand = [];
-    this.winnings = 0;
-    this.takenAction = false;
-    this.revealed = false;
-  }
-
-  get id() {
-    return this.tablePlayer.id;
-  }
-
-  get chips() {
-    return this.tablePlayer.chips;
-  }
-
-  get #currentBet() {
-    return this.#currentBet;
-  }
-
-  resetCurrentBet() {
-    this.#currentBet = 0;
-  }
-
-  canTakeAction() {
-    return !this.folded && this.tablePlayer.chips > 0;
-  }
-
-  callTo(amount: number) {
-    const diff = Math.max(amount - this.#currentBet, 0);
-    this.raiseBet(diff);
-  }
-
-  raiseBet(amount: number) {
-    const maxAmount = Math.min(this.tablePlayer.chips, amount);
-
-    this.tablePlayer.takeChips(maxAmount);
-    this.#currentBet += maxAmount;
-  }
-
-  giveChips(amount: number) {
-    this.tablePlayer.giveChips(amount);
   }
 }
